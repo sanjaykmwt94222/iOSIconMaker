72e90039b41039db3db7bc6834b1c163a23d8182
Sanjay Kumawat <sanjay.kumawat@hpslabdesigns.com>
2020-09-14 12:32:31 +0530

    this commit fixes #3

--- iosIconMaker.sh
+++ iosIconMaker.sh
@@ -15,17 +15,20 @@ mkdir -p $folder
 
 resizeImg () {
 	width=$1
-	sips -z $width $width         	  $source --out $folder"/icon_"$width"x"$width".png"
-	sips -z $((width*2)) $((width*2)) $source --out $folder"/icon_"$width"x"$width"@2x.png"
-	sips -z $((width*3)) $((width*3)) $source --out $folder"/icon_"$width"x"$width"@3x.png"
+	sips -z $width $width -s format png $folder"/temp.bmp" --out $folder"/icon_"$width"x"$width".png"
+	sips -z $((width*2)) $((width*2)) -s format png $folder"/temp.bmp" --out $folder"/icon_"$width"x"$width"@2x.png"
+	sips -z $((width*3)) $((width*3)) -s format png $folder"/temp.bmp" --out $folder"/icon_"$width"x"$width"@3x.png"
 }
 
 sizesVariants=(20 29 40 50 57 60 72 76)
 
+sips -s format bmp $source --out $folder"/temp.bmp"
 for item in ${sizesVariants[*]}
 do
   resizeImg $item
 done
 
-sips -z 167 167   $source --out $folder"/icon_83.5x83.5@2x.png"
-sips -z 1024 1024 $source --out $folder"/icon_1024x1024.png"
+sips -z 167 167   -s format png $folder"/temp.bmp" --out $folder"/icon_83.5x83.5@2x.png"
+sips -z 1024 1024 -s format png $folder"/temp.bmp" --out $folder"/icon_1024x1024.png"
+
+rm -rf $folder"/temp.bmp"
